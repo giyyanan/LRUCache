20ddc5acc5d844aad313f0044c3c57610b98b26a
GIV <giyyanan@andrew.cmu.edu>
2016-05-24 11:02:03 -0400

    Doubly linked List class added

---
+++ DLL.py
@@ -0,0 +1,141 @@
+class Node(object):
+	
+	def __init__(self,value):
+		self.value = value
+		self.prev = None
+		self.next = None
+
+#DoublyLinkedList
+class DLL(object):
+
+	def __init__(self,value):
+		self.first = Node(value)
+		self.last = self.first
+		self.size = 1
+
+	def getFirst(self):
+		return self.first
+
+	def getLast(self):
+		return self.last
+
+	def getSize(self):
+		return self.size
+
+	def setFirst(self,value):
+		newNode = Node(value)
+
+		newNode.next = self.first
+		newNode.prev = self.first.prev
+
+		self.first.prev = newNode
+
+		self.first = newNode
+
+		self.size+=1
+
+	def setLast(self,value):
+		newNode = Node(value)
+
+		newNode.next = self.last.next
+		newNode.prev = self.last
+
+		self.last.next = newNode
+
+		self.last = newNode
+
+		self.size+=1
+
+	def removeFirst(self):
+
+		if(self.size >1):
+			tempNode = self.first.next
+
+			tempNode.prev = self.first.prev
+			self.first.next = None
+
+			self.first = tempNode
+
+			self.size-=1
+
+		else:
+			print "cannot remove the only available Node"
+
+	def removeLast(self):
+
+		if(self.size >1):
+			tempNode = self.last.prev
+
+			tempNode.next = self.last.next
+			self.last.prev = None
+
+			self.last = tempNode
+
+			self.size-=1
+
+		else:
+			print "cannot remove the only available Node"
+
+	def remove(self,value):
+
+		tempNode = self.first
+
+		if self.first.value == value:
+			self.removeFirst()
+
+		elif self.last.value == value:
+			self.removeLast()
+
+		else:
+
+			found = True
+			while tempNode.value != value:
+				if(tempNode.next is None):
+					print "value not found"
+					found = False
+					break
+				else:
+					tempNode = tempNode.next
+
+			if(found):
+				tempNode.prev.next = tempNode.next
+				tempNode.next.prev = tempNode.prev
+
+				tempNode.prev = None
+				tempNode.next = None
+				self.size-=1
+
+	def find(self,value):
+		found = False
+
+		tempNode = self.first
+
+		if self.first.value == value:
+			found = True
+
+		elif self.last.value == value:
+			found = True
+
+		else:
+			while tempNode.next is not None:
+				if tempNode.value == value:
+					found = True
+				tempNode = tempNode.next
+
+		return found
+	
+	def printList(self):
+
+		tempNode = self.first
+		
+		while tempNode.next is not None:
+			print tempNode.value
+			tempNode = tempNode.next
+
+		print tempNode.value
+
+
+
+
+
+
